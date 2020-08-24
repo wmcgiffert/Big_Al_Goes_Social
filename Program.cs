69688bab144ac92ff879dd31c3da962b48671221
wmcgiffert <wmcgiffert@crimson.ua.edu>
2020-08-24 15:20:39 -0500

    class video update

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bigAlGoesSocial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
