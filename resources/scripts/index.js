ea17cc1cfe9fb7953e6d0cfa2ce7bc2e531d4193
wmcgiffert <wmcgiffert@crimson.ua.edu>
2020-10-07 15:23:23 -0500

    JavaScript started

---
+++ resources/scripts/index.js
@@ -0,0 +1,19 @@
+var tweets = ["Hey there from Big AL"+ "Hey again" + "It's still me"];
+
+setGreets = function(){
+    var html = "<ul>";
+    tweets.forEach((tweets)=>{html += "<li>"
+    });
+}
+<ul>
+<li>
+    <div class="avatar"></div><span>Hey there, this is Big AL</span>
+</li>
+<li>
+    <div class="avatar"></div><span>It's me again</span>
+</li>
+<li>
+    <div class="avatar"></div><span>Hope you are enjoying 321</span>
+</li>
+
+</ul>
